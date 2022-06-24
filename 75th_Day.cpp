e96617dfbcfe35a575f352c926057c29732e8bd2
Harsh Govind <harsh5092001@gmail.com>
2022-06-24 22:18:45 +0530

    Created 75th Day
    https://www.codechef.com/JUNE222D/problems/PAIREQ

---
+++ 75th_Day.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        unordered_map<int, int> m;
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            m[x]++;
+        }
+        int mx = INT_MIN;
+        for (auto &val : m)
+        {
+            // cout << val.first << " " << val.second;
+            mx = max(mx, val.second);
+        }
+        cout << n - mx << endl;
+    }
+    return 0;
+}
